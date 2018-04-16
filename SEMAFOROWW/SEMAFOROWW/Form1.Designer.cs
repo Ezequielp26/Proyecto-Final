dfde02e0dc7100e3849d3273d6b1cf2c21cdef2b
Ezequiel Patricio <36133758+Ezequielp26@users.noreply.github.com>
2018-04-16 00:29:22 -0400

    Add files via upload

---
+++ SEMAFOROWW/SEMAFOROWW/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace SEMAFOROWW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnEncender = new System.Windows.Forms.Button();
+            this.btnDetener = new System.Windows.Forms.Button();
+            this.btnApagar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(227, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ezequiel Patricio Seccion 0908 02-SISN-1-230";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SEMAFOROWW.Properties.Resources.SEMAFORO_ROJO;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 116);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnEncender
+            // 
+            this.btnEncender.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncender.Location = new System.Drawing.Point(80, 48);
+            this.btnEncender.Name = "btnEncender";
+            this.btnEncender.Size = new System.Drawing.Size(79, 23);
+            this.btnEncender.TabIndex = 3;
+            this.btnEncender.Text = "Encender";
+            this.btnEncender.UseVisualStyleBackColor = true;
+            this.btnEncender.Click += new System.EventHandler(this.btnEncender_Click);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDetener.Location = new System.Drawing.Point(80, 98);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(79, 23);
+            this.btnDetener.TabIndex = 4;
+            this.btnDetener.Text = "Detener";
+            this.btnDetener.UseVisualStyleBackColor = true;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // btnApagar
+            // 
+            this.btnApagar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApagar.Location = new System.Drawing.Point(80, 141);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(79, 23);
+            this.btnApagar.TabIndex = 5;
+            this.btnApagar.Text = "Apagar";
+            this.btnApagar.UseVisualStyleBackColor = true;
+            this.btnApagar.Click += new System.EventHandler(this.btnApagar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnApagar);
+            this.Controls.Add(this.btnDetener);
+            this.Controls.Add(this.btnEncender);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnEncender;
+        private System.Windows.Forms.Button btnDetener;
+        private System.Windows.Forms.Button btnApagar;
+    }
+}
+
---
+++ SEMAFOROWW/SEMAFOROWW/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SEMAFOROWW
+{
+    public partial class Form1 : Form
+    {
+        int caso = 0;
+        private object picturebox1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = SEMAFOROWW.Properties.Resources.SEMAFORO_APAGO;
+            btnApagar.Enabled = false;
+            btnDetener.Enabled = false;
+
+        }
+
+        private void btnEncender_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            btnEncender.Enabled = false;
+            btnDetener.Enabled = true;
+            btnApagar.Enabled = true;
+            label1.Text = "Semaforo Encendido";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            switch (caso)
+            {
+                case 0:
+                    pictureBox1.Image = SEMAFOROWW.Properties.Resources.SEMAFORO_VERDE;
+                    timer1.Interval = 5000;
+                    caso = 1;
+                    break;
+                case 1:
+                    pictureBox1.Image = SEMAFOROWW.Properties.Resources.SEMAFORO_AMARILLO;
+                    timer1.Interval = 2000;
+                    caso = 2;
+                    break;
+                case 2:
+                    pictureBox1.Image = SEMAFOROWW.Properties.Resources.SEMAFORO_ROJO;
+                    timer1.Interval = 5000;
+                    caso = 0;
+                    break;
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            if (btnDetener.Text == "Detener")
+            {
+                timer1.Enabled = false;
+                btnDetener.Text = "Reanudar";
+                label1.Text = "Semaforo Detenido";
+                
+            }
+
+            else
+            if (btnDetener.Text == "Reanudar")
+
+
+            {
+
+                timer1.Enabled = true;
+                btnDetener.Text = "Detener";
+                label1.Text = "Semaforo Encendido";
+            }
+
+        }
+
+        private void btnApagar_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            btnDetener.Enabled = false;
+            btnApagar.Enabled = false;
+            btnEncender.Enabled = true;
+            pictureBox1.Image = SEMAFOROWW.Properties.Resources.SEMAFORO_APAGO;
+            label1.Text = "Semaforo Apagado";
+
+          
+        }
+    }
+}
+
+
